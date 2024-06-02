177aad118bdf489799cbb7b7a97663b6e289d6fc
tonyfilho <oficinadosdrones@gmail.com>
2024-06-02 14:59:14 +0100

    second commit

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, ContentComponent, RouterOutlet],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
